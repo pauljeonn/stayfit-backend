9edbf83b975e5847a88e8d281e02d80b6b531c45
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-02-08 23:32:21 +0900

    Feat: MongoDB 커넥션

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log('Connected to MongoDB.'))
+	.catch((err) => console.log(err));
 
 app.listen(4000, () => {
 	console.log('Backend server is running.');
