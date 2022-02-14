3c88c99b614747eecc98d4a288fd3be9b944fea8
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-02-14 20:37:40 +0900

    Feat: Exercise 모델 생성

---
+++ models/Exercise.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const ExerciseSchema = new mongoose.Schema(
+	{
+		userId: {
+			type: String,
+			require: true,
+		},
+		title: {
+			type: String,
+			require: true,
+		},
+		desc: {
+			type: String,
+			max: 50,
+		},
+		days: {
+			type: Array,
+			default: [],
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model('Exercise', ExerciseSchema);
