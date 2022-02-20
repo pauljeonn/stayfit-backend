475b2684d587f7c1b5028f9e6d6cd0b1b2677177
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-02-21 05:38:33 +0900

    Feat: 운동 삭제(Delete) 메서드 추가

--- routes/exercises.js
+++ routes/exercises.js
@@ -13,7 +13,6 @@ router.get('/', async (req, res) => {
 
 // 운동 생성
 router.post('/', async (req, res) => {
-	console.log(req.body);
 	const newExercise = new Exercise(req.body);
 	try {
 		const savedExercise = await newExercise.save();
@@ -29,6 +28,16 @@ router.put('/:id', async (req, res) => {
 		const exercise = await Exercise.findById(req.params.id);
 		const editedExercise = await exercise.updateOne({ $set: req.body });
 		res.status(200).json(editedExercise);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+// 운동 삭제
+router.delete('/:id', async (req, res) => {
+	try {
+		const deletedExercise = await Exercise.deleteOne({ _id: req.params.id });
+		res.status(200).json(deletedExercise);
 	} catch (err) {
 		res.status(500).json(err);
 	}
