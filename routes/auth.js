92fd4fcd987ce7d26e3b66bbc894997154be5e43
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-03-06 23:57:54 +0900

    Feat: LOGIN 메서드

--- routes/auth.js
+++ routes/auth.js
@@ -23,4 +23,26 @@ router.post('/register', async (req, res) => {
 	}
 });
 
+// LOGIN
+router.post('/login', async (req, res) => {
+	try {
+		const user = await User.findOne({ email: req.body.email });
+		!user && res.status(400).send('해당 이메일 계정이 없습니다.');
+
+		// bcrypt compare 메서드 사용해서 비밀번호 비교
+		const validPassword = await bcrypt.compare(
+			req.body.password,
+			user.password
+		);
+		!validPassword && res.status(400).send('잘못된 비밀번호입니다.');
+
+		// 비밀번호만 제외하고 유저 데이터 반환
+		const { password, ...other } = user._doc;
+
+		res.status(200).json(other);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
